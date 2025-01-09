fa7df5d02c9aa63472e7f1b888339cba4de8503d
Martín Stier <mastier@umich.edu>
2025-01-09 01:01:04 -0300

    close...

--- index.js
+++ index.js
@@ -5,17 +5,26 @@ dropButtons.forEach(button => {
         const targetID = button.getAttribute("data-target");
         const targetElement = document.getElementById(targetID);
 
-        if (targetElement.style.display === "none" || targetElement.style.display === "") {
-            button.innerHTML = '<i class="fa-solid fa-caret-down"></i>&nbsp;&nbsp;' + button.textContent.trim();
-            targetElement.style.display = "block";
-        }
+        const mainElement = document.querySelector("main");
+        const footerElement = document.querySelector("footer");
 
-        else {
-            targetElement.style.display = "none";
-            button.innerHTML = '<i class="fa-solid fa-caret-right"></i>&nbsp;&nbsp;' + button.textContent.trim();
-        }
-    }
-    )
-}
-)
+        const mainDropButtons = mainElement.querySelectorAll(".drop-button");
+        mainDropButtons.forEach(mainButton => {
+            const mainTargetID = mainButton.getAttribute("data-target");
+            const mainTargetElement = document.getElementById(mainTargetID);
 
+            if (mainButton !== button) {
+                footerElement.appendChild(mainButton);
+                footerElement.appendChild(mainTargetElement);
+
+                mainTargetElement.style.display = "none";
+                mainButton.innerHTML = '<i class="fa-solid fa-caret-right"></i>&nbsp;&nbsp;' + mainButton.textContent.trim();
+            }
+        });
+
+        mainElement.appendChild(button);
+        mainElement.appendChild(targetElement);
+        button.innerHTML = '<i class="fa-solid fa-caret-down"></i>&nbsp;&nbsp;' + button.textContent.trim();
+        targetElement.style.display = "block";
+    });
+});
