76afb7ccd4a3298ea8add1cd3839e025adafe601
Martín Stier <mastier@umich.edu>
2025-01-12 23:21:48 -0300

    -

--- index.js
+++ index.js
@@ -8,18 +8,6 @@ dropButtons.forEach(button => {
 
         const selectedElement = document.querySelector(".selected-drop");
 
-        selectedElement.innerHTML = '';
-
-        // Hide any existing drop buttons and their target elements in the selected section
-        const selectedDropButtons = selectedElement.querySelectorAll(".drop-button");
-        selectedDropButtons.forEach(selectedButton => {
-            const selectedTargetID = selectedButton.getAttribute("data-target");
-            const selectedTargetElement = document.getElementById(selectedTargetID);
-
-            selectedTargetElement.style.display = "none";
-            selectedButton.innerHTML = '<i class="fa-solid fa-caret-right"></i>&nbsp;&nbsp;' + selectedButton.textContent.trim();
-        });
-
         selectedElement.innerHTML = ''; // Clear the selected section
 
         const buttonClone = button.cloneNode(true);
@@ -27,7 +15,7 @@ dropButtons.forEach(button => {
         selectedElement.appendChild(buttonClone);
         selectedElement.appendChild(targetElementClone);
 
-        buttonClone.innerHTML = '<i class="fa-solid fa-caret-down"></i>&nbsp;&nbsp;' + buttonClone.textContent.trim();
+        buttonClone.innerHTML = '<i class="fa-solid fa-caret-down"></i>&nbsp;&nbsp;<b>' + buttonClone.textContent.trim() + '</b>';
         targetElementClone.style.display = "block";
     });
 });
